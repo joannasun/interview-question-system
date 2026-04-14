4d2f86dfb5c56d7ee95aa84dddb16b7f85c325ef
joanna <1554146891@qq.com>
2026-04-14 17:56:40 +0800

    'update'

--- backend/app.py
+++ backend/app.py
@@ -32,8 +32,8 @@ app.include_router(router)
 #     import uvicorn
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import uvicorn
+#     import os
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
